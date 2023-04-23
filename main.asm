c36b839ad942f30616f44bce183bca1f1328c5ca
[C]Phong- <chauphongnguyenhuu@gmail.com>
2023-04-23 20:17:39 +0700

    open display

--- main.asm
+++ main.asm
@@ -3,17 +3,29 @@ section .text
 
     global _start
 
+    ;-------------------------------------------------------------
+    ; open a connection to the X server that controls a display
+    ; 
+    ; @param rdi
+    ;   specifies the hardware display name.
+    ;   if it is NULL, the DISPLAY environment variable will be used
+    ; 
+    ; @return rax
+    ;   return the pointer of Display structure.
+    ;   if it does not succeed, it return NULL
+    ;-------------------------------------------------------------
+    extern XOpenDisplay
+
 _start:
-    mov     rax, 1
-    mov     rdi, 1
-    mov     rsi, hello
-    mov     rdx, 14
-    syscall
-    
-    mov     rax, 60     ; exit program
+    xor     rdi, rdi        ; use DISPLAY environment variable
+    call    XOpenDisplay
+
+    mov     rax, 60         ; exit program
     xor     rdi, rdi
     syscall
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 section .data
-    hello: db "hello, world", 0ah, 0h
+    display: dq 0h
+
+
